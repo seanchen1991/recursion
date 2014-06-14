147286ee112cc0f8f8eb92fe47c59549628e8f06
Sean Chen <skypemaster007@gmail.com>
2014-06-14 14:28:51 -0700

    Added comments to parseJSON.js

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,9 +3,13 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  if (json === "") {
+  if (json === "") {  //if json object is empty, return an empty string
   	json = '""';
   }
-  eval("var parsed =" + json + ";");
-  return parsed;
+  eval("var parsed =" + json + ";");  //the eval function evaluates JS strings
+  return parsed;	
 };
+
+/*create a string with json inside, then call eval on this string and returns the 
+object corresponding to the passed in JSON text
+*/
